0cc354fd941bd9c4f560c1ecf9f2d766df46650e
jpegoraro <34581927+jpegoraro@users.noreply.github.com>
2018-05-07 20:57:35 +0200

    ticks

--- HW3/grafici.m
+++ HW3/grafici.m
@@ -9,7 +9,9 @@ awgn_bound = 4*(1-1/sqrt(M))*qfunc(sqrt(SNR_lin/(sigma_a/2)));
 
 
 figure,
-plot(SNR, 10*log10(Perr),'<-'), hold on, plot(SNR, 10*log10(awgn_bound))
+plot(SNR, 10*log10(Perr),'b--'), hold on, plot(SNR, 10*log10(awgn_bound),'g')
 ylim([10*log10(10^-4) 10*log10(10^-1)])
+yticks([-40 -30 -20 -10])
+yticklabels({'10^{-4}','10^{-3}','10^{-2}','10^{-1}'})
 xlabel('SNR [dB]')
 ylabel('P_e')
