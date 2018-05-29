ec81ea5a56ba85d60ffa0645da3ca82525eeca6c
jpegoraro <34581927+jpegoraro@users.noreply.github.com>
2018-05-29 15:58:40 +0200

    added a figure

--- HW4/prova1.m
+++ HW4/prova1.m
@@ -9,8 +9,8 @@ load('generated_symbols.mat','a','enc_b_l','b_l','uncoded_a')
 % [Pbit_OFDM_coded(i) b_l_hat] = OFDM_coded(a, b_l, 11, 91, 10^(SNR_dB(i)/10));
 % end
 
-% SNR_dB = 1.2;
-% [Pbit_OFDM_coded b_l_hat] = OFDM_coded(a, b_l, 11, 91, 10^(SNR_dB/10));
+SNR_dB = 1.2;
+[Pbit_OFDM_coded b_l_hat] = OFDM_coded(a, b_l, 11, 91, 10^(SNR_dB/10));
 
 % SNR_dB = 1.1;
 % for i=7:20
@@ -26,10 +26,10 @@ load('generated_symbols.mat','a','enc_b_l','b_l','uncoded_a')
 % end
 
 
-SNR_dB = [4:0.5:14];
-for i=1:length(SNR_dB)
-[Pbit_OFDM_uncoded(i) b_l_hat] = OFDM_uncoded(uncoded_a, b_l, 11, 91, 10^(SNR_dB(i)/10));
-end
+% SNR_dB = [4:0.5:14];
+% for i=1:length(SNR_dB)
+% [Pbit_OFDM_uncoded(i) b_l_hat] = OFDM_uncoded(uncoded_a, b_l, 11, 91, 10^(SNR_dB(i)/10));
+% end
 %save('OFDM_uncoded.mat','SNR_dB','Pbit_OFDM_uncoded1');
 % 
 
---
+++ HW4/raised_cosine_prova.m
@@ -0,0 +1,20 @@
+clear all; close all; clc;
+set(0,'defaultTextInterpreter','latex')
+ro = 0.0625;
+span = 30;
+sps = 4;
+g_rcos = rcosdesign(ro, span, sps, 'sqrt');
+
+[H f] = freqz(g_rcos, 1);
+figure, plot(f/(0.5*pi), 10*log10(abs(H)))
+xlim([0 1])
+ylim([-20 7])
+xlabel('$f/T_c$')
+ylabel('$|G_{\sqrt{rcos}|} [dB]$')
+% figure, stem(g_rcos)
+% figure, stem(conv(g_rcos,g_rcos))
+% 
+% gdown = downsample(conv(g_rcos,g_rcos),4);
+% figure, stem(gdown)
+% [Hdown fd] = freqz(gdown, 1, 'whole');
+% figure, plot(fd/pi, abs(Hdown)), ylim([0 3])
