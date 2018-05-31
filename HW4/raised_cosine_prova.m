36d0b55472271edf33f23305858f5f58c44059e6
jpegoraro <34581927+jpegoraro@users.noreply.github.com>
2018-05-31 12:58:31 +0200

    report continues

--- HW4/raised_cosine_prova.m
+++ HW4/raised_cosine_prova.m
@@ -5,16 +5,16 @@ span = 30;
 sps = 4;
 g_rcos = rcosdesign(ro, span, sps, 'sqrt');
 
-[H f] = freqz(g_rcos, 1);
-figure, plot(f/(0.5*pi), 10*log10(abs(H)))
-xlim([0 1])
-ylim([-20 7])
+[H f] = freqz(g_rcos, 1,'whole',4);
+figure, plot(f/(2*pi), 10*log10(abs(H)))
+% xlim([0 0.5])
+% ylim([-20 7])
 xlabel('$f/T_c$')
 ylabel('$|G_{\sqrt{rcos}|} [dB]$')
-figure, stem(g_rcos)
-figure, stem(conv(g_rcos,g_rcos))
-
-gdown = downsample(conv(g_rcos,g_rcos),4);
-figure, stem(gdown)
-[Hdown fd] = freqz(gdown, 1, 'whole');
-figure, plot(fd/pi, abs(Hdown)), ylim([0 3])
+% figure, stem(g_rcos)
+% figure, stem(conv(g_rcos,g_rcos))
+% 
+% gdown = downsample(conv(g_rcos,g_rcos),4);
+% figure, stem(gdown)
+% [Hdown fd] = freqz(gdown, 1, 'whole');
+% figure, plot(fd/pi, abs(Hdown)), ylim([0 3])
