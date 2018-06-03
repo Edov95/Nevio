2939ccd05f3383201295ff1baf2cd2480c8661f3
jpegoraro <34581927+jpegoraro@users.noreply.github.com>
2018-06-03 14:31:38 +0200

    final

--- HW4/prova1.m
+++ HW4/prova1.m
@@ -5,17 +5,17 @@ load('generated_symbols.mat','a','enc_b_l','b_l','uncoded_a')
 %parpool;
 
 %% CODED
-SNR_dB = [0.9:0.05:1.6];
-parfor i=1:length(SNR_dB)
-    [Pbit_OFDM_coded(i) b_l_hat] = ... 
-        OFDM_coded(a, b_l, 18, 43, 10^(SNR_dB(i)/10));
-end
+% SNR_dB = [0.9:0.05:1.6];
+% parfor i=1:length(SNR_dB)
+%     [Pbit_OFDM_coded(i) b_l_hat] = ... 
+%         OFDM_coded(a, b_l, 18, 43, 10^(SNR_dB(i)/10));
+% end
 
 % save('OFDM_coded.mat','Pbit_OFDM_coded')
 
-% SNR_dB = 1.3;
-% [Pbit_OFDM_coded b_l_hat] = ... 
-%OFDM_coded(a, b_l, 18, 43, 10^(SNR_dB/10));
+SNR_dB = 1.3;
+[Pbit_OFDM_coded b_l_hat] = ... 
+OFDM_coded(a, b_l, 18, 43, 10^(SNR_dB/10));
 
 % SNR_dB = 1.3;
 % parfor i=1:60
